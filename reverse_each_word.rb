8f1e653551af916f6f8f992817ef5907e33acb1d
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 17:41:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  string.split(' ')
+  array = string.split(' ')
   result = ''
-  string.each do |word|
+  array.each do |word|
     result << word.reverse 
   end 
   result
